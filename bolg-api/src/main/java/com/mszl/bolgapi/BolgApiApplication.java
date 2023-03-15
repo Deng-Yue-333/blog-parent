c334bc4b4523e8d048f746257f5fb50e22dd9f7b
Dylan-macos <2375224554@qq.com>
2023-03-16 00:19:17 +0800

    更新父项目的pom文件

---
+++ bolg-api/src/main/java/com/mszl/bolgapi/BolgApiApplication.java
@@ -0,0 +1,13 @@
+package com.mszl.bolgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolgApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolgApiApplication.class, args);
+    }
+
+}
